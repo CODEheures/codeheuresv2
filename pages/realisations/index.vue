8b684706d76e2f0e926eb6c1e6bca99d2f2eb751
CODEheures <webmaster@codeheures.fr>
2018-06-26 17:47:53 +0200

    selfjob before destockeurope

--- pages/realisations/index.vue
+++ pages/realisations/index.vue
@@ -6,6 +6,21 @@
     <div class="row-cards">
       <realisation
         :card="{
+          title: 'à venir: Selfjob',
+          img: 'selfjob.jpg',
+          links: [
+            {innerText: 'Nouveau site', href: 'https://selfjob.codeheures.fr'},
+            {innerText: 'Vidéo', href: 'https://youtu.be/ZklNr5oyn4A'}
+          ]
+        }"
+      >
+        <p slot="description" class="description">Cette future application, permet un recrutement sans CV basé sur l'affinité des personnalités.
+          <br>Initialement réalisée avec <a class="a-invert-light" href="http://quasar-framework.org">Quasar</a> et <a class="a-invert-light" href="https://laravel.com">Laravel</a>
+          une nouvelle version utilisant l'excellent framework <a class="a-invert-light" href="https://vuetifyjs.com/en/">Vuetify</a> est en cours de développement
+        </p>
+      </realisation>
+      <realisation
+        :card="{
           title: 'Destockeurope.com',
           img: 'destockeurope.jpg',
           links: [
@@ -15,21 +30,6 @@
       >
         <p slot="description" class="description">Cette application, compatible sur support mobile,  permet la mise en ligne d'annonces de destockage entre professionnels.
           <br>Réalisée avec <a class="a-invert-light" href="https://laravel.com">Laravel</a>, <a class="a-invert-light" href="https://vuejs.org/">VueJS</a> et <a class="a-invert-light" href="https://semantic-ui.com/">SemanticUI</a>
-        </p>
-      </realisation>
-      <realisation
-        :card="{
-          title: 'à venir: Selfjob',
-          img: 'selfjob.jpg',
-          links: [
-            {innerText: 'Vidéo', href: 'https://youtu.be/ZklNr5oyn4A'},
-            {innerText: 'Nouveau site', href: 'https://selfjob.codeheures.fr'}
-          ]
-        }"
-      >
-        <p slot="description" class="description">Cette future application, permet un recrutement sans CV basé sur l'affinité des personnalités.
-          <br>Initialement réalisée avec <a class="a-invert-light" href="http://quasar-framework.org">Quasar</a> et <a class="a-invert-light" href="https://laravel.com">Laravel</a>
-          une nouvelle version utilisant l'excellent framework <a class="a-invert-light" href="https://vuetifyjs.com/en/">Vuetify</a> est en cours
         </p>
       </realisation>
     </div>
